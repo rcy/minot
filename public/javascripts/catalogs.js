5e719ee6f44037ca97e8150ad07e8d0837d8ef02
Ryan Yeske <rcyeske@gmail.com>
2012-11-17 11:21:36 -0800

    reorder functions

--- public/javascripts/catalogs.js
+++ public/javascripts/catalogs.js
@@ -11,23 +11,28 @@ App.CatalogView = Backbone.View.extend({
   tagName: 'li',
   className: 'catalog',
   template: _.template('<a href="#"><%= name %></a>'),
+
   events: {
     "click a": "click"
   },
+
+  initialize: function(options) {
+    this.model.on('change', this.render, this);
+  },
+
   click: function() {
     console.log('click', this.model.get('name'));
   },
+
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
     return this;
-  },
-  initialize: function(options) {
-    this.model.on('change', this.render, this);
   }
 });
 
 App.CatalogsView = Backbone.View.extend({
   template: _.template("<ul></ul>"),
+
   render: function() {
     var $el = this.$el;
     $el.html(this.template());
@@ -38,6 +43,7 @@ App.CatalogsView = Backbone.View.extend({
     }, this);
     return this;
   },
+
   initialize: function(options) {
     this.collection.on('reset', this.render, this);
   }
